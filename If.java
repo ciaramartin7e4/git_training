485d0fd325cf93caeef91b94a55b26b61a067b98
CiaraMartin <ciara.martin.7e4@itb.cat>
2020-11-05 12:03:40 +0100

    Hem afegit If.java

---
+++ If.java
@@ -0,0 +1,10 @@
+public class If{
+	public static void main (String[] args){
+		boolean a = true;
+		if(a == true){
+			System.out.println("Hola");
+		}else{
+			System.out.println("Adéu");
+		}
+	}
+}
